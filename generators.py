7ac3f181b4c2f2ad7eafe2bd8e890e0b33896f01
Get <villegas.get@gmail.com>
2024-11-03 01:39:25 -0600

    Refactor tree_from_sequence

--- generators.py
+++ generators.py
@@ -3,34 +3,62 @@ import networkx as nx
 import numpy as np
 
 def generate_prufer_sequence(n_nodes):
+    """
+    Genera una secuencia de Prüfer aleatoria.
+    """
     return np.random.randint(0, n_nodes, size=n_nodes - 2)
 
-def tree_from_sequence(sequence, add_positions=True):
-    edges = []
-    n_nodes = sequence.shape[0] + 2
+def calculate_degrees(sequence, n_nodes):
+    """
+    Calcula los grados iniciales de los nodos basados en la secuencia de Prüfer.
+    """
     degrees = np.ones(n_nodes)
-
-
     counts = np.bincount(sequence, minlength=n_nodes)
     degrees += counts
+    return degrees
 
-    for a in sequence:
-        origin = np.argwhere(degrees == 1)[0, 0]
-        edges.append((a, origin))
-        degrees[origin] -= 1
-        degrees[a] -= 1
+def add_edge(edges, node1, node2, degrees):
+    """
+    Añade una arista al árbol y actualiza los grados de los nodos.
+    """
+    edges.append((node1, node2))
+    degrees[node1] -= 1
+    degrees[node2] -= 1
 
-    remaining = np.argwhere(degrees == 1)[:, 0]
-    assert remaining.shape[0] == 2, "There are more than 2 remaining degrees = 1"
+def generate_positions(n_nodes, edges):
+    """
+    Genera posiciones 3D para los nodos del árbol utilizando NetworkX
+    """
+    tree = nx.Graph()
+    tree.add_nodes_from(np.arange(n_nodes))
+    tree.add_edges_from(edges)
+    return nx.drawing.layout.fruchterman_reingold_layout(tree, dim=3, scale=1.)
 
-    edges.append((remaining[1], remaining[0]))
+def tree_from_sequence(sequence, add_positions=True):
+    """
+    Crea un árbol a partir de una secuencia de Prüfer.
+    """
+
+    n_nodes = sequence.shape[0] + 2
+    degrees = calculate_degrees(sequence, n_nodes)
+    
+    edges = []
+
+    # Construye las aristas a partir de la secuencia
+    for node in sequence:
+        leaf = np.argwhere(degrees == 1)[0, 0]
+        add_edge(edges, node, leaf, degrees)
+
+    # Agrega la última arista
+    remaining_nodes = np.argwhere(degrees == 1)[:, 0]
+    assert remaining_nodes.shape[0] == 2, "There are more than 2 remaining degrees = 1"
+    edges.append((remaining_nodes[1], remaining_nodes[0]))
+
+    # Genera posiciones de ser necesario
     positions = None
 
     if add_positions:
-        tree = nx.Graph()
-        tree.add_nodes_from(np.arange(n_nodes))
-        tree.add_edges_from(edges)
-        positions = nx.drawing.layout.fruchterman_reingold_layout(tree, dim=3, scale=1.)
+        positions = generate_positions(n_nodes, edges)
         
     return Tree(np.arange(n_nodes), np.array(edges), positions)
 

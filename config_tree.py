b46640d504dd569a6549cbb25187bcb427487062
Get <villegas.get@gmail.com>
2025-01-31 01:47:15 -0600

    Adding config tree

---
+++ config_tree.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+nodes = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+edges = [(0, 1), (0, 2), (0, 3), (0, 4), 
+         (1, 5), (1, 6), 
+         (2, 8), (2, 9),
+         (3, 11),
+         (6, 7),
+         (9, 10),
+         (11, 12),
+         (12, 13),
+         (13, 14)  ]  # Conexiones entre nodos
+
+# Definir posiciones específicas para los nodos
+nodes_positions = [
+    [0, 0, 15],   # Nodo 0
+    [0, -4, 12],   # Nodo 1
+    [4, 0, 12],  # Nodo 2
+    [-4, 0, 12],   # Nodo 3
+    [0, 4, 12],    # Nodo 4
+    [0, -6, 9],   # Nodo 5
+    [0, -2, 9],   # Nodo 6
+    [0, -2, 6],  # Nodo 7
+    [6, 0, 9],   # Nodo 8
+    [2, 0, 9],    # Nodo 9
+    [2, 0, 6],   # Nodo 10
+    [-4, 0, 9],   # Nodo 11
+    [-4, 0, 6],  # Nodo 12
+    [-4, 0, 3],   # Nodo 13
+    [-4, 0, 0]    # Nodo 14
+]
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import time
-import numpy as np
 from src.python.tree_utils import Tree
 from src.python.visualizer import TreeVisualizer
 from src.python.fire_simulation import FirePropagation
+from config_tree import nodes, edges, nodes_positions
 
 start_time = time.perf_counter()
 
@@ -31,37 +31,6 @@ def simulate_fire(my_tree, visualizer):
     run_fire_simulation(fire, visualizer)
 
     visualizer.plot_3d_final_state(burning_nodes, burned_nodes, protected_nodes)
-
-# Crear un árbol de ejemplo
-nodes = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
-edges = [(0, 1), (0, 2), (0, 3), (0, 4), 
-         (1, 5), (1, 6), 
-         (2, 8), (2, 9),
-         (3, 11),
-         (6, 7),
-         (9, 10),
-         (11, 12),
-         (12, 13),
-         (13, 14)  ]  # Conexiones entre nodos
-
-# Definir posiciones específicas para los nodos
-nodes_positions = [
-    [0, 0, 15],   # Nodo 0
-    [0, -4, 12],   # Nodo 1
-    [4, 0, 12],  # Nodo 2
-    [-4, 0, 12],   # Nodo 3
-    [0, 4, 12],    # Nodo 4
-    [0, -6, 9],   # Nodo 5
-    [0, -2, 9],   # Nodo 6
-    [0, -2, 6],  # Nodo 7
-    [6, 0, 9],   # Nodo 8
-    [2, 0, 9],    # Nodo 9
-    [2, 0, 6],   # Nodo 10
-    [-4, 0, 9],   # Nodo 11
-    [-4, 0, 6],  # Nodo 12
-    [-4, 0, 3],   # Nodo 13
-    [-4, 0, 0]    # Nodo 14
-]
 
 # Crear una instancia de Tree
 my_tree = Tree(nodes, edges, nodes_positions)

e975a6809ac25ee3af469c987072111005e7d6e5
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2025-02-27 23:12:12 -0600

    Adding visualize func

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -7,31 +7,31 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 from visualizer import TreeVisualizer
 from greedy.simulation import Simulation
 from config_tree import my_tree, root
+
+visualizer = TreeVisualizer(my_tree)
+
+def vizualize_state(simulation, step):
+    burning_nodes = simulation.state.burning_nodes
+    burned_nodes = simulation.state.burned_nodes
+    protected_nodes = simulation.state.protected_nodes
+    visualizer.plot_fire_state(burning_nodes, burned_nodes, step, protected_nodes, simulation.firefighter.position)
    
 def execute_experiment():
-    visualizer = TreeVisualizer(my_tree)
+    step = 0
     visualizer.plot_3d_tree(my_tree, "images/initial_tree")
     simulation = Simulation(my_tree)
     simulation.start_fire(root)
-    burned_nodes = simulation.state.burned_nodes
-    burning_nodes = simulation.state.burning_nodes
-    protected_nodes = simulation.state.protected_nodes
-    visualizer.plot_fire_state(burning_nodes, burned_nodes, 0, protected_nodes, simulation.firefighter.position)
+    vizualize_state(simulation, step)
     
-    step = 0
-
     while not simulation.is_completely_burned():
         step += 1
         simulation.select_node_to_protect()
         simulation.propagate()
         print(f"Paso {step}")
-        burning_nodes = simulation.state.burning_nodes
-        burned_nodes = simulation.state.burned_nodes
-        protected_nodes = simulation.state.protected_nodes
-        visualizer.plot_fire_state(burning_nodes, burned_nodes, step, protected_nodes, simulation.firefighter.position)
+        vizualize_state(simulation, step)
 
-    visualizer.plot_3d_final_state(burning_nodes, burned_nodes, protected_nodes, simulation.firefighter.position)
-    print('-' * 50 + f"\nDaño: {len(burned_nodes) + len(burning_nodes)}\n" + '-' * 50)
+    visualizer.plot_3d_final_state(simulation.state.burning_nodes, simulation.state.burned_nodes, simulation.state.protected_nodes, simulation.firefighter.position)
+    print('-' * 50 + f"\nDaño: {len(simulation.state.burned_nodes) + len(simulation.state.burning_nodes)}\n" + '-' * 50)
 
 def main():
     start_time = time.perf_counter()

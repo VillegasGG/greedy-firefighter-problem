dd7d60879285d733b301dc7ee89bc972becfe626
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2025-04-08 00:46:40 -0600

    Moving functions to other class

--- src/greedy/greedy_step.py
+++ src/greedy/greedy_step.py
@@ -85,6 +85,97 @@ class GreedyStep():
                     layer[int(neighbor)] = layer[s] + 1
 
         return layer
+    
+    def get_final_candidates(self, candidates, fire_time, time_ff_reach, env):
+
+        final_candidates = set()
+
+        # Filter candidates that can be reached before the fire
+        for candidate in candidates:
+            time_ff_reach_candidate = time_ff_reach[candidate]
+            time_to_burn_candidate = fire_time[candidate]
+            remaining_time = env.firefighter.get_remaining_time()
+            if time_ff_reach_candidate > time_to_burn_candidate:
+                continue
+            elif remaining_time < 1:
+                next_step_burn = time_to_burn_candidate - 1
+                next_step_ff = time_ff_reach_candidate - remaining_time
+                if next_step_ff < next_step_burn:
+                    final_candidates.add((candidate, time_ff_reach[candidate]))
+                else:
+                    # print(f'IMPORTANT!!  -- Node: {candidate} | Time to reach: {time_ff_reach[candidate]} | Time to burn: {fire_time[candidate]} but remaining time is {remaining_time}')
+                    continue
+            else:
+                if time_ff_reach[candidate] < time_to_burn_candidate:
+                    final_candidates.add((candidate, time_ff_reach[candidate]))
+
+        return final_candidates
+    
+    def is_protected_by_ancestor(self, node, env):
+        path = env.tree.get_path_to_root(node)
+        for ancestor in path:
+            if ancestor in env.state.protected_nodes:
+                return True
+        return False
+
+    def get_not_protected_nodes(self, env):
+        candidates = set()
+
+        unnafected_nodes = set(env.tree.nodes) - env.state.burned_nodes - env.state.protected_nodes - env.state.burning_nodes
+
+        for element in unnafected_nodes:
+            is_protected = self.is_protected_by_ancestor(element, env)
+            if not is_protected:
+                candidates.add(element)
+
+        return candidates
+
+    def get_candidates(self, env):
+
+        first_candidates = self.get_not_protected_nodes(env)
+
+        ff_distances = env.firefighter.get_distances_to_nodes(first_candidates)
+        fire_time = self.steps_to_reach_all()
+
+        time_ff_reach = {} # Time taken to reach each candidate
+        for candidate in first_candidates:
+            time_ff_reach[candidate] = ff_distances[candidate] / env.firefighter.speed
+      
+        # self.show_candidates("Candidates after first filter (After protected by ancestor):", first_candidates, fire_time, time_ff_reach)
+
+        final_candidates = self.get_final_candidates(first_candidates, fire_time, time_ff_reach, env)
+        
+        # self.show_candidates_tuple("Final candidates:", final_candidates, fire_time)
+
+        return final_candidates
+
+    def select_node_to_protect_and_move(self, env):
+        """
+        - Seleccion de un nodo a proteger: se selecciona el nodo con el subarbol mas grande (aunque este mas lejos)
+        - Se mueve el bombero al nodo seleccionado
+        """
+        burned_and_burning_nodes = env.state.burned_nodes.union(env.state.burning_nodes)
+        self.burned_nodes = burned_and_burning_nodes
+        candidates = self.get_candidates(env)
+        node_to_protect, node_time = self.get_node_to_protect(candidates, env.firefighter)
+        print(f'Node to protect: {node_to_protect} | Time to reach: {node_time}')
+        
+        if node_to_protect:
+            node_pos = env.tree.nodes_positions[node_to_protect]
+            if(env.firefighter.get_remaining_time() >= node_time):
+                env.state.protected_nodes.add(node_to_protect)
+                env.firefighter.move_to_node(node_pos, node_time)
+                env.firefighter.protecting_node = None
+            else:
+                env.firefighter.move_fraction(node_pos)
+                env.firefighter.protecting_node = node_to_protect
+                
+            env.firefighter.print_info()
+            return True
+
+        else:
+            print('No node to protect')
+            return False
 
 
 
--- src/greedy/simulation.py
+++ src/greedy/simulation.py
@@ -1,19 +1,10 @@
 from greedy.greedy_step import GreedyStep
-from firefighter import Firefighter
-from fire_state import FireState
 from greedy.environment import Environment
 
 class Simulation:
     def __init__(self, tree):
         self.env = Environment(tree)
         self.greedy = GreedyStep(tree)
-
-    def is_protected_by_ancestor(self, node):
-        path = self.env.tree.get_path_to_root(node)
-        for ancestor in path:
-            if ancestor in self.env.state.protected_nodes:
-                return True
-        return False
 
     def show_candidates(self, message, candidates, fire_time, time_ff_reach=None):
         print('-' * 50)
@@ -30,90 +21,6 @@ class Simulation:
             print(f'Node: {candidate[0]} | Time to reach: {candidate[1]} | Fire time: {fire_time[candidate[0]]}')
         print('-' * 50)
 
-    def get_not_protected_nodes(self):
-        candidates = set()
-
-        unnafected_nodes = set(self.env.tree.nodes) - self.env.state.burned_nodes - self.env.state.protected_nodes - self.env.state.burning_nodes
-
-        for element in unnafected_nodes:
-            is_protected = self.is_protected_by_ancestor(element)
-            if not is_protected:
-                candidates.add(element)
-
-        return candidates
-
-    def get_final_candidates(self, candidates, fire_time, time_ff_reach):
-
-        final_candidates = set()
-
-        # Filter candidates that can be reached before the fire
-        for candidate in candidates:
-            time_ff_reach_candidate = time_ff_reach[candidate]
-            time_to_burn_candidate = fire_time[candidate]
-            remaining_time = self.env.firefighter.get_remaining_time()
-            if time_ff_reach_candidate > time_to_burn_candidate:
-                continue
-            elif remaining_time < 1:
-                next_step_burn = time_to_burn_candidate - 1
-                next_step_ff = time_ff_reach_candidate - remaining_time
-                if next_step_ff < next_step_burn:
-                    final_candidates.add((candidate, time_ff_reach[candidate]))
-                else:
-                    # print(f'IMPORTANT!!  -- Node: {candidate} | Time to reach: {time_ff_reach[candidate]} | Time to burn: {fire_time[candidate]} but remaining time is {remaining_time}')
-                    continue
-            else:
-                if time_ff_reach[candidate] < time_to_burn_candidate:
-                    final_candidates.add((candidate, time_ff_reach[candidate]))
-
-        return final_candidates
-
-    def get_candidates(self):
-
-        first_candidates = self.get_not_protected_nodes()
-
-        ff_distances = self.env.firefighter.get_distances_to_nodes(first_candidates)
-        fire_time = self.greedy.steps_to_reach_all()
-
-        time_ff_reach = {} # Time taken to reach each candidate
-        for candidate in first_candidates:
-            time_ff_reach[candidate] = ff_distances[candidate] / self.env.firefighter.speed
-      
-        # self.show_candidates("Candidates after first filter (After protected by ancestor):", first_candidates, fire_time, time_ff_reach)
-
-        final_candidates = self.get_final_candidates(first_candidates, fire_time, time_ff_reach)
-        
-        # self.show_candidates_tuple("Final candidates:", final_candidates, fire_time)
-
-        return final_candidates
-
-    def select_node_to_protect_and_move(self):
-        """
-        - Seleccion de un nodo a proteger: se selecciona el nodo con el subarbol mas grande (aunque este mas lejos)
-        - Se mueve el bombero al nodo seleccionado
-        """
-        burned_and_burning_nodes = self.env.state.burned_nodes.union(self.env.state.burning_nodes)
-        self.greedy.burned_nodes = burned_and_burning_nodes
-        candidates = self.get_candidates()
-        node_to_protect, node_time = self.greedy.get_node_to_protect(candidates, self.env.firefighter)
-        print(f'Node to protect: {node_to_protect} | Time to reach: {node_time}')
-        
-        if node_to_protect:
-            node_pos = self.env.tree.nodes_positions[node_to_protect]
-            if(self.env.firefighter.get_remaining_time() >= node_time):
-                self.env.state.protected_nodes.add(node_to_protect)
-                self.env.firefighter.move_to_node(node_pos, node_time)
-                self.env.firefighter.protecting_node = None
-            else:
-                self.env.firefighter.move_fraction(node_pos)
-                self.env.firefighter.protecting_node = node_to_protect
-                
-            self.env.firefighter.print_info()
-            return True
-
-        else:
-            print('No node to protect')
-            return False
-
     def firefighter_action(self):
         """
         Turno del bombero
@@ -121,7 +28,7 @@ class Simulation:
         exist_candidate = True
 
         while(self.env.firefighter.get_remaining_time() > 0 and exist_candidate):
-            exist_candidate = self.select_node_to_protect_and_move()
+            exist_candidate = self.greedy.select_node_to_protect_and_move(self.env)
             
     def execute_step(self):
         """
